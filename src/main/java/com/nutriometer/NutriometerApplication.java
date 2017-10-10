a931720115ad146ab5717f02bffb3f759a973087
Laszlo Bulatovic <laci96zenta@gmail.com>
2017-10-10 10:00:31 +0200

    First commit

---
+++ src/main/java/com/nutriometer/NutriometerApplication.java
@@ -0,0 +1,12 @@
+package com.nutriometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriometerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriometerApplication.class, args);
+	}
+}
