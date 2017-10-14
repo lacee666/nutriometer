70091cd8c0077dad9ba6ef6796ea207a5338edad
Laszlo Bulatovic <laci96zenta@gmail.com>
2017-10-14 11:18:52 +0200

    more fixes on foodsearch.html tables

---
+++ src/main/resources/static/ingredientloader.js
@@ -0,0 +1,10 @@
+function addElement() {
+	var table = document.getElementById("ingredients");
+	var row = table.insertRow(table.rows.length);
+
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+
+	cell1.innerHTML = "NEW CELL1";
+	cell2.innerHTML = "NEW CELL2";
+}
